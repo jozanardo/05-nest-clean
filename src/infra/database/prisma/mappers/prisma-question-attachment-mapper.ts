6f8487ecba7c05c029c011344ad1a5decc65c3a8
João Zanardo <jozanardo@gmail.com>
2025-01-06 20:14:54 -0300

    feat: persist attachments in the database

--- src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -1,4 +1,4 @@
-import { Attachment as PrismaAttachment } from '@prisma/client'
+import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 export class PrismaQuestionAttachmentMapper {
@@ -13,5 +13,24 @@ export class PrismaQuestionAttachmentMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: QuestionAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.attachmentId.toString()
+    })
+
+    return {
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+      data: {
+        questionId: attachments[0].questionId.toString(),
+      },
+    }
   }
 }
