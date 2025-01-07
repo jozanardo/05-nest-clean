cddf54f6bbe6abc4cdf376b65b730cb051411404
João Zanardo <jozanardo@gmail.com>
2025-01-07 18:41:33 -0300

    feat: edit questions with attachments

--- test/factories/make-question-attachments.ts
+++ test/factories/make-question-attachments.ts
@@ -1,4 +1,6 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 import {
   QuestionAttachment,
@@ -19,4 +21,26 @@ export function makeQuestionAttachment(
   )
 
   return questionAttachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachment(
+    data: Partial<QuestionAttachmentProps> = {},
+  ): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: questionAttachment.attachmentId.toString(),
+      },
+      data: {
+        questionId: questionAttachment.questionId.toString(),
+      },
+    })
+
+    return questionAttachment
+  }
 }
